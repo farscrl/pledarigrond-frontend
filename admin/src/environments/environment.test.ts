06a06c554338b2a5cc36d767944d8c43b13c462d
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-03-30 17:15:46 +0200

    fix: url

--- admin/src/environments/environment.test.ts
+++ admin/src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://pledarigrond-api-test.hidora.com/',
+  apiUrl: 'https://pledarigrond-api-test.hidora.com',
   apiHost: 'pledarigrond-api-test.hidora.com',
   frontendUrl: 'https://pledarigrond-test.hidora.com',
 };
--- frontend/src/environments/environment.test.ts
+++ frontend/src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://pledarigrond-api-test.hidora.com/',
+  apiUrl: 'https://pledarigrond-api-test.hidora.com',
   apiHost: 'pledarigrond-api-test.hidora.com',
   adminBackendUrl: 'https://pledarigrond-test.hidora.com/admin',
 };
