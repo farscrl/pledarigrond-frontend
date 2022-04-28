e220c851bc4612cc802d80bbcbdb8d6d4a67e764
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-04-28 17:41:23 +0200

    fix: backend url

--- frontend/src/environments/environment.test.ts
+++ frontend/src/environments/environment.test.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://pg-api.pre.far.ch',
   apiHost: 'pg-api.pre.far.ch',
-  adminBackendUrl: 'https://pg-api.pre.far.ch/admin',
+  adminBackendUrl: 'https://pg.pre.far.ch/admin',
 };
