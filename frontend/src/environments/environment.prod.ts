878f06df48df5132226bf52d288ba838a59d850e
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-06-16 12:13:02 +0200

    fix: add slash to backend urls to fix redirect issue
    without the slash, nginx redirected the url to port 8080

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.pledarigrond.ch',
   apiHost: 'api.pledarigrond.ch',
-  adminBackendUrl: 'https://www.pledarigrond.ch/admin',
+  adminBackendUrl: 'https://www.pledarigrond.ch/admin/',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
   apiHost: 'localhost:8080',
-  adminBackendUrl: 'http://localhost:4222',
+  adminBackendUrl: 'http://localhost:4222/',
 };
 
 /*
