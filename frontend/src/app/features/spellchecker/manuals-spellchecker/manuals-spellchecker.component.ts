4a4defa9053c5079116698b7a6cb35f8998fbf40
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-11-14 11:43:57 +0100

    feat: import surmiran/german texts

--- frontend/src/app/features/spellchecker/manuals-spellchecker/manuals-spellchecker.component.ts
+++ frontend/src/app/features/spellchecker/manuals-spellchecker/manuals-spellchecker.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SimpleModalComponent } from "ngx-simple-modal";
+import {FrontendLanguage, SelectedLanguageService} from "../../../services/selected-language.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-manuals-spellchecker',
@@ -11,11 +13,24 @@ export class ManualsSpellcheckerComponent extends SimpleModalComponent<{manualTy
   @Input()
   manualType?: ManualType;
 
-  constructor() {
+  frontEndLanguage: FrontendLanguage = 'rm';
+
+  private languageSubscription?: Subscription;
+
+  constructor(private selectedLanguageService: SelectedLanguageService) {
     super();
   }
 
   ngOnInit(): void {
+    this.languageSubscription = this.selectedLanguageService.getFrontendLanguageObservable().subscribe(value => {
+      this.frontEndLanguage = value;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.languageSubscription) {
+      this.languageSubscription.unsubscribe();
+    }
   }
 
   downloadHunspell() {
