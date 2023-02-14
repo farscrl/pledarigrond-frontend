f26e34c23583a5d7c01064090d0ecfd8e5de85bb
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-02-14 14:38:37 +0100

    feat: allow to show diffs of version history

--- admin/src/app/components/version-history/version-history.component.ts
+++ admin/src/app/components/version-history/version-history.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, OnInit, ViewContainerRef } from '@angular/core';
 import { LemmaVersion } from 'src/app/models/lemma-version';
-import { LexEntryUi } from 'src/app/models/lex-entry';
+import {LexEntry, LexEntryUi} from 'src/app/models/lex-entry';
 
 import * as moment from 'moment';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { TranslateService } from '@ngx-translate/core';
 import { EditorService } from 'src/app/services/editor.service';
 import { LanguageSelectionService } from 'src/app/services/language-selection.service';
+import {DiffModalComponent} from "../../features/diff-modal/diff-modal.component";
 
 @Component({
   selector: 'app-version-history',
@@ -26,6 +27,9 @@ export class VersionHistoryComponent implements OnInit {
   selectedLexEntry?: LexEntryUi;
 
   versionHistory: readonly LemmaVersion[] = [];
+
+  diffOldLemmaVersion: LemmaVersion | undefined;
+  diffNewLemmaVersion: LemmaVersion | undefined;
 
   constructor(
     private modalService: NzModalService,
@@ -58,6 +62,42 @@ export class VersionHistoryComponent implements OnInit {
       },
       nzOnOk: () => this.dropOutdatedHistoryConfirmed(),
       nzOnCancel: () => {}
+    });
+  }
+
+  beforeChanged(lemmaVersion?: LemmaVersion) {
+    if (!lemmaVersion) {
+      lemmaVersion = new LemmaVersion();
+    }
+    this.diffOldLemmaVersion = lemmaVersion;
+  }
+
+  afterChanged(lemmaVersion?: LemmaVersion) {
+    if (!lemmaVersion) {
+      lemmaVersion = new LemmaVersion();
+    }
+    this.diffNewLemmaVersion = lemmaVersion;
+  }
+
+  showDiff() {
+    // diff view is implemented to take lex entry. Thus, we create a new lexEntry with the lemmaVersions to compare.
+    const lexEntry = new LexEntry();
+    lexEntry.current =  JSON.parse(JSON.stringify(this.diffOldLemmaVersion!));
+    lexEntry.current.internalId = 0;
+    lexEntry.mostRecent =JSON.parse(JSON.stringify(this.diffNewLemmaVersion!));
+    lexEntry.mostRecent.internalId = 1;
+
+    const modal = this.modalService.create({
+      nzTitle: this.translateService.instant('edit.titles.edit'),
+      nzContent: DiffModalComponent,
+      nzClosable: true,
+      nzMaskClosable: true,
+      nzWidth: 1100,
+      nzFooter: null,
+      nzViewContainerRef: this.viewContainerRef,
+      nzComponentParams: {
+        lexEntry: lexEntry,
+      },
     });
   }
 
