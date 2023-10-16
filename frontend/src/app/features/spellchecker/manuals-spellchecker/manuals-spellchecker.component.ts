0498cfe1c03fea20e5aa45a1193d6d639e2eb842
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-10-16 17:11:36 +0200

    feat: add spellchecker manuals for rumgr

--- frontend/src/app/features/spellchecker/manuals-spellchecker/manuals-spellchecker.component.ts
+++ frontend/src/app/features/spellchecker/manuals-spellchecker/manuals-spellchecker.component.ts
@@ -33,5 +33,5 @@ export class ManualsSpellcheckerComponent extends NgxModalComponent<{manualType:
 }
 
 export type ManualType = 'macos' | 'hunspell' | 'word';
-export type ManualLanguage = 'de' | 'surmiran' | 'sutsilvan' | 'rumantschgrischun';
-export type HunspellLanguage = 'rm-surmiran' | 'rm-sutsilv';
+export type ManualLanguage = 'de' | 'surmiran' | 'sutsilvan' | 'rumantschgrischun' | 'puter' | 'vallader';
+export type HunspellLanguage = 'rm-surmiran' | 'rm-sutsilv' | 'rm-rumgr' | 'rm-puter' | 'rm-vallader';
