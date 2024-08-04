4b705f8320d8acb7ffbe91b990fb3bb97d3c9ea0
Gion-Andri Cantieni <gion-andri@gmx.ch>
2024-08-04 15:08:47 +0200

    feat: improve pronoun removing for sursilvan

--- admin/src/app/components/lemma-diff/lemma-diff.component.ts
+++ admin/src/app/components/lemma-diff/lemma-diff.component.ts
@@ -41,4 +41,33 @@ export class LemmaDiffComponent implements OnInit{
       this.oldLemmaVersion = this.lexEntry.current;
     }
   }
+
+  removeSurilvanPronouns(value?: string): string|undefined {
+    if (value === undefined) {
+      return undefined;
+    }
+
+    const prefixes = ['che ', 'ch\'', 'jeu ', 'ti ', 'el/ella ', 'i ', 'nus ', 'vus ', 'els/ellas ']
+    const singleForms: string[] = value.split(/\r?\n|\r|\u0085|\u2028|\u2029/);
+    for (let i = 0; i < singleForms.length; i++) {
+      if (singleForms[i].trim() === '') {
+        continue;
+      }
+      let enclosedInBrackets = false;
+      if (singleForms[i].charAt(0) === '(' && singleForms[i].charAt(singleForms[i].length - 1) === ')') {
+        enclosedInBrackets = true;
+        singleForms[i] = singleForms[i].replace("(", "").replace(")", "");
+      }
+      for (const p of prefixes) {
+        if (singleForms[i].startsWith(p)) {
+          singleForms[i] = singleForms[i].substring(p.length).trim();
+        }
+      }
+      singleForms[i] = singleForms[i].replace(/!$/, '').trim();
+      if (enclosedInBrackets) {
+        singleForms[i] = "(" + singleForms[i] + ")";
+      }
+    }
+    return singleForms.join("\n");
+  }
 }
