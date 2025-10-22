ceac50d7d925a392d344b0af04583f0f04161bb6
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-10-22 12:07:43 +0200

    chore: update ngx-translate

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -17,7 +17,7 @@ import { FooterComponent } from './components/footer/footer.component';
 export class AppComponent {
   constructor(translate: TranslateService, private swUpdate: SwUpdate) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('rm-rumgr');
+    translate.setFallbackLang('rm-rumgr');
 
      // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('rm-rumgr');
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,10 @@ import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { UserLoggedInGuard } from './app/auth/logged-in.guard';
 import { UserNotLoggedInGuard } from './app/auth/not-logged-in.guard';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LanguageUtils } from './app/utils/language-utils';
 import { MatomoTrackClickDirective, provideMatomo, withRouter } from 'ngx-matomo-client';
-import { provideTranslateService, TranslateDirective, TranslateLoader, TranslatePipe } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateService, TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
 import { FormsModule } from '@angular/forms';
@@ -15,18 +14,12 @@ import { defaultNgxModalOptions, NgxModalView } from 'ngx-modalview';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { TiptapEditorDirective } from 'ngx-tiptap';
 import { AppComponent } from './app/app.component';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 const TOKEN_KEY = 'jwt';
 export function tokenGetter() {
   return localStorage.getItem(TOKEN_KEY);
 }
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
-
-
 
 if (environment.production) {
   enableProdMode();
@@ -65,12 +58,8 @@ bootstrapApplication(AppComponent, {
             trackerUrl: environment.matomoTrackingUrl,
         }, withRouter()),
         provideTranslateService({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            },
-            defaultLanguage: 'rm-rumgr'
+            loader: provideTranslateHttpLoader({prefix:'./assets/i18n/', suffix:'.json'}),
+            fallbackLang: 'rm-rumgr'
         })
     ]
 })
