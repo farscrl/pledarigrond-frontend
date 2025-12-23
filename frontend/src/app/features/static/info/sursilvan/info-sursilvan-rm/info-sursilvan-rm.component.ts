ca22d283c6228e731f7842f9fbc1657cc760e894
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-12-23 11:48:10 +0100

    feat: update info page for sursilvan

--- frontend/src/app/features/static/info/sursilvan/info-sursilvan-rm/info-sursilvan-rm.component.ts
+++ frontend/src/app/features/static/info/sursilvan/info-sursilvan-rm/info-sursilvan-rm.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { ThousandSeparatorPipe } from '../../../../../pipes/thousand-separator.pipe';
 
 @Component({
-  selector: 'app-info-sursilvan-rm',
-  templateUrl: './info-sursilvan-rm.component.html',
-  styleUrl: './info-sursilvan-rm.component.scss'
+    selector: 'app-info-sursilvan-rm',
+    templateUrl: './info-sursilvan-rm.component.html',
+    imports: [
+        ThousandSeparatorPipe
+    ],
+    styleUrl: './info-sursilvan-rm.component.scss'
 })
 export class InfoSursilvanRmComponent {
   @Input()
