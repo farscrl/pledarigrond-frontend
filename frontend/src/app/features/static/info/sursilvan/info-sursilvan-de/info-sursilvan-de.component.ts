3f66cd4f3befb45d9afe4160be9bf7aefc3ba56d
Gion-Andri Cantieni <gion-andri@gmx.ch>
2026-01-08 09:28:29 +0100

    chore: german info text for pg sursilvan

--- frontend/src/app/features/static/info/sursilvan/info-sursilvan-de/info-sursilvan-de.component.ts
+++ frontend/src/app/features/static/info/sursilvan/info-sursilvan-de/info-sursilvan-de.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { ThousandSeparatorPipe } from '../../../../../pipes/thousand-separator.pipe';
 
 @Component({
   selector: 'app-info-sursilvan-de',
   templateUrl: './info-sursilvan-de.component.html',
-  styleUrl: './info-sursilvan-de.component.scss'
+  styleUrl: './info-sursilvan-de.component.scss',
+  imports: [
+    ThousandSeparatorPipe
+  ]
 })
 export class InfoSursilvanDeComponent {
   @Input()
